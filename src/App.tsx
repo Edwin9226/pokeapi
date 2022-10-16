78d4f88cdea9949837403623ab91628b6b85448d
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-10-16 10:41:57 -0500

    create vite app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello world.</h1>
+      <h2>Tis is my first component.</h2>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const divStyle= {color: 'red' , height:30};
+function Home() {
+  return (
+    <div style={divStyle}>Home Bienvenido</div>
+  )
+}
+
+export default Home
