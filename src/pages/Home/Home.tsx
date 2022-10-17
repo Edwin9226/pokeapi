c4bd06740fb661a34d8e78199894c533cf73fa7e
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-10-17 09:44:05 -0500

    React hooks usestaless

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,9 +9,16 @@ const props =()=>{
 
 function Home(props:any) {
   const [lastname, setLastname] = useState('Pinchao');
+  const [count, setCount]= useState(0);
   return (
-    <div style={divStyle}>Home Bienvenido {props.user} {lastname}</div>
-  )
+    <div style={divStyle}>Home Bienvenido {props.user} {lastname}
+    <p>Counter ={count}</p>
+    <button onClick={ () => setCount(count+1)}> 
+    Increment
+    </button>
+    </div>
+    
+    )
 }
 
 export default Home
