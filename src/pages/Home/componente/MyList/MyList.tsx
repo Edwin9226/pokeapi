12a1066128182ff4b58fde600ccceab912719a01
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-10-17 16:00:11 -0500

    Handling forms

--- src/pages/Home/componente/MyList/MyList.tsx
+++ src/pages/Home/componente/MyList/MyList.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function MyList() {
     const data=[1,2,3,4,5];
+    const [text, setText]= useState('');
+    // Save input element value to state when it has been changed
+    const inputChanged =(event: any) =>{
+        alert('You typed: ${text}');
+        event.preventDefault();
+    }
+
+    const handleSubmit= (event: any)=>{
+        alert('You typed: ${text}');
+        event.preventDefault();
+    }
+
   return (
     <div>
         <ul>
@@ -12,7 +24,13 @@ function MyList() {
             </li>
            )
         }
-        </ul></div>
+        </ul>
+    <form onSubmit={handleSubmit}>
+        <input type="text" onChange={event => setText(event.target.value)} value={text} />
+
+        <input  type="submit" value="Press me"/>
+    </form>    
+    </div>
   )
 }
 
